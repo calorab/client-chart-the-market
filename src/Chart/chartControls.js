f790731ea6ed5ebe31c4d15af83a52b16a86b1c4
Caleb Gammon <calebwgammon@gmail.com>
2020-09-14 19:13:55 -0400

    set up basic Form with Formik pre CSS and API

--- src/Chart/chartControls.js
+++ src/Chart/chartControls.js
@@ -1,15 +1,43 @@
 import React, {Component} from 'react'
 import Aux from '../../hoc/Aux/Aux'
+import {Formik, Field, Form, useField, useFormikContext} from 'formik'
 
 class ChartControls extends Component {
       
     render() {
         return (
             <Aux>
+
                 {/* 
-                    create form with Formik - include check boxes etc and in 2 columns
-                    Add buttons for search and reset controls(??)
+                CHART HERE!!
+
+                NOTE:
+                    Using AnyChart charting API and Alpha Vantage API
                 */}
+                
+                <Formik initialValues={{symbol: "", check: []}} onSubmit={alert("Sumbitted")} >
+                    <Form>
+                        <div className='stockSymbol' name='symbol'>
+                            <Field />
+                        </div>
+                        <div classname='checkboxes' role='group'>
+                            <label>Analysis Tools</label>
+                            <label>
+                                <Field type='checkbox' name='check' value='macd' />
+                            </label>
+                            <label>
+                                <Field type='checkbox' name='check' value='stoich' />
+                            </label>
+                            <label>
+                                <Field type='checkbox' name='check' value='etc1' />
+                            </label>
+                            <label>
+                                <Field type='checkbox' name='check' value='etc2' />
+                            </label>   
+                        </div>
+                    </Form>
+                    <button className='chartSubmitButton'>Submit</button>
+                </Formik>
             </Aux>
         )
     }
