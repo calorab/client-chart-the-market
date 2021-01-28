d4bd93cf9bb7ae19f646a43c84ee3886814aefd6
Caleb Gammon <calebwgammon@gmail.com>
2021-01-27 19:25:57 -0500

    finished basic form for tickerSymbol

--- src/Chart/chartControls.js
+++ src/Chart/chartControls.js
@@ -1,38 +1,41 @@
 import React, {Component} from 'react'
 import Aux from '../../hoc/Aux/Aux'
-import {Formik, Field, Form, useField, useFormikContext} from 'formik'
+import {Formik, Field, Form, ErrorMessage} from 'formik'
 import * as Yup from 'yup';
-import { blanchedalmond } from 'color-name';
 
 class ChartControls extends Component {
-      
+
+    state = {
+        ticker: "",
+        submitting: false,
+
+    }
+
+    onSubmitHandler = () => {
+        alert("Submitted!");
+    }
+
     render() {
 
-        const formTicker = 
+        const form = 
             <Formik
-                initialValues={{}}
-                validationSchema={Yup.object()}
-                onSubmit={(bla, blah) => {}}
+                initialValues={{tickerSymbol: ""}}
+                validationSchema={Yup.object({tickerSymbol: Yup.string().required('Required')})}
+                onSubmit={() => {
+                    this.onSubmitHandler()
+                }}
             >
                 <Form>
-
-                </Form>
-            </Formik>;
-
-        const formMain = 
-            <Formik
-                initialValues = {{}}
-                validationSchema = {Yup.object()}
-            >
-                <Form>
-
+                    <label htmlFor="tickerSymbol">Search for a Ticker Symbol</label>
+                    <Field name="tickerSymbol" type="text" />
+                    <ErrorMessage name="tickerSymbol" />
+                    <button type="submit">Search</button>
                 </Form>
             </Formik>;
 
         return (
             <Aux>
-                {formTicker}
-                {formMain}
+                {form}
             </Aux>
         )
     }
