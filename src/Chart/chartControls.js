04090b05e73237cdac1ae37ceabcedae5047737c
Caleb Gammon <calebwgammon@gmail.com>
2021-02-01 20:02:04 -0500

    symbol fetch to state done, started mapping state for results display

--- src/Chart/chartControls.js
+++ src/Chart/chartControls.js
@@ -7,30 +7,47 @@ class ChartControls extends Component {
     state = {
         ticker: "",
         submitting: false,
+        companyResults: [],
+    }
+
+   
+    
+    onTickerSubmitHandler = async event => {
+        this.setState({submitting: true})
+        // let keyword = event.target.tickerSymbol.value
+        let apiEndpoint = "http://localhost:8000/symbol/stocksymbol?keyword=ibm" 
+
+        let response = await fetch(apiEndpoint, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const matches = await response.json();
+        // console.log(matches.bestMatches)
+        this.setState({companyResults: matches.bestMatches, submitting: false})
+        // console.log(this.state.companyResults)
+    }
+
+    onChartSubmithandler = async event => {
 
     }
 
-    onSubmitHandler = async () => {
-        let result = await this.testForHandler()
-        console.log(result);
-        return result
-    }
-
-    testForHandler() {
-        console.log('test');
-        return 'finished'
-    }
-
+    
     render() {
 
-        
+        const symbolResults = this.state.companyResults.map(element => {
+            return <div>
+                        <h3>{element.name}</h3>
+                        <button type='submit' onClick={this.onChartSubmithandler()}>Get chart data for this company</button>                       
+                    </div>
+        })
 
         const form = 
             <Formik
                 initialValues={{tickerSymbol: ""}}
                 validationSchema={Yup.object({tickerSymbol: Yup.string().required('Required')})}
                 onSubmit={() => {
-                    this.onSubmitHandler()
+                    this.onTickerSubmitHandler()
                 }}
             >
                 <Form>
@@ -44,6 +61,7 @@ class ChartControls extends Component {
         return (
             <div>
                 {form}
+                {symbolResults}
             </div>
 
             // <p>Hello World!</p>
