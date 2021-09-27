354f8a4ade199e17ff50906d539e48b9bcf67bbb
Caleb Gammon <calebwgammon@gmail.com>
2021-09-27 17:22:49 -0400

    added and commented out code adjustments for weekly data - data size limiting the  weeky price usage for now

--- src/Chart/chartMain.js
+++ src/Chart/chartMain.js
@@ -1,9 +1,10 @@
-import React, {Component} from 'react'
-import {Formik, Field, Form, ErrorMessage} from 'formik'
+import React, {Component} from 'react';
+import {Formik, Field, Form, ErrorMessage} from 'formik';
 import * as Yup from 'yup';
-import AnyChart from 'anychart-react'
-import anychart from 'anychart'
-import dataMapping from '../utility/dataMapping'
+import AnyChart from 'anychart-react';
+import anychart from 'anychart';
+import dataMapping from '../utility/dataMapping';
+import Spinner from '../spinner/spinner';
 
 class ChartMain extends Component {
 
@@ -32,8 +33,8 @@ class ChartMain extends Component {
     tickerSearchHandler = async event => {
         event.preventDefault()
 
-        this.setState({submitting: true})
-        // console.log("Event: ", event, "Event.target.tickerSymbol", event.target.tickerSymbol)
+        this.setState({submitting: true, showChart: false})
+        
         let keyword = event.target.tickerSymbol.value;
         let apiEndpoint = "http://localhost:8000/symbol/stocksymbol?keyword=" + keyword
 
@@ -49,21 +50,27 @@ class ChartMain extends Component {
 
     priceHandler = async event => {
         event.preventDefault();
-
+        
         let symbol = event.target.equitySymbol.value
         let EMALow = event.target.lowEMAInterval.value
         let EMAHigh = event.target.highEMAInterval.value
-
-        let priceEndpoint = "http://localhost:8000/chartdata?symbol=" + symbol
+        // let interval = event.target.interval.value 
+// for below when weekly data issue resolved... : + "&interval=" + interval
+        let priceEndpoint = "http://localhost:8000/chartdata?symbol=" + symbol 
 
         let response = await fetch(priceEndpoint, {
             headers: {
                 'Content-Type': 'application/json'
-            }
+            },
         })
 
         let fullData = await response.json()
         let priceData = fullData["Time Series (Daily)"];
+        if (!fullData["Time Series (Daily)"]) {
+            priceData = fullData['Weekly Adjusted Time Series']; 
+        }
+
+        console.log(priceData);
         
         const dataTable = dataMapping(priceData);
 
@@ -130,33 +137,36 @@ class ChartMain extends Component {
                     <Field name="highEMAInterval" type="number" />
                     <ErrorMessage name="highEMAInterval" />
 
-                    <label>Interval Period</label>
+                    {/* <label htmlFor="interval">Interval Period</label>
                     <Field name="interval" as="select">
                         <option value="daily">Daily</option>
                         <option value="weekly">Weekly</option>
-                    </Field>
+                    </Field> */}
     
                     <button type="submit">Search</button>
                 </Form>
             </Formik>;
 
-                // create table
-                let dataTable = anychart.data.table('date');
-                // add data
-                dataTable.addData(this.state.equityTable);
-                // mapAs
-                let mapping = dataTable.mapAs({x: 'date', value: 'price'});
-                // create chart
-                let chart = anychart.stock();
-                // add series
-                let series = chart.plot(0).column(mapping);
-                // --- technical indicators ---
-                chart.plot(0).ema(mapping, this.state.emaLow, "line");
-                chart.plot(0).ema(mapping, this.state.emaHigh, "line");
-                chart.plot(1).macd(mapping); // need to change height to be smaller - 50% probs.
-                // ----- end technical indicators ---
-                series.name(`${this.state.ticker}`);
-                chart.draw();
+        // ----------  Anychart Table and Chart Config ----------
+        // create table
+        let dataTable = anychart.data.table('date');
+        // add data
+        dataTable.addData(this.state.equityTable);
+        // mapAs
+        let mapping = dataTable.mapAs({x: 'date', value: 'price'});
+        // create chart
+        let chart = anychart.stock();
+        // add series
+        let series = chart.plot(0).column(mapping);
+
+        // --- technical indicators ---
+        chart.plot(0).ema(mapping, this.state.emaLow, "line");
+        chart.plot(0).ema(mapping, this.state.emaHigh, "line");
+        chart.plot(1).macd(mapping); // need to change height to be smaller - 50% probs. ----  .height('50%') ... let macd_plot = 
+        // ----- end technical indicators ---
+
+        series.name(`${this.state.ticker}`);
+        chart.draw();
             
         
             // {/* ChartWindow component needed for the graph? 
@@ -181,7 +191,7 @@ class ChartMain extends Component {
                 height={600}
                 instance={chart}
                 title={`100-Day ${this.state.ticker} chart with EMA's of ${this.state.emaLow} & ${this.state.emaHigh}`}
-                /></div> : null}
+                /></div> : <Spinner />}
             </div>
             
             
---
+++ src/spinner/spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './spinner.css';
+
+const spinner = () => (
+    <div className="lds-facebook"></div>
+);
+
+export default spinner;
