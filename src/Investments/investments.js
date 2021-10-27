8e8527d5ca75ddefd3fb97aa8169b8969693ee46
Caleb Gammon <calebwgammon@gmail.com>
2021-10-27 11:48:56 -0400

    tested post and "get" for sales endpoint no display yet

--- src/Investments/investments.js
+++ src/Investments/investments.js
@@ -20,10 +20,11 @@ class Investments extends Component {
             this.props.history.push('/auth');
         }
         this.getInvestmentsHandler();
+        this.getSalesHandler();
     }
 
     getInvestmentsHandler = async () => {
-        const investmentEndpoint = 'https://pure-ridge-03326.herokuapp.com/myinvestments';
+        const investmentEndpoint = 'http://localhost:8000/myinvestments';
         let response = await fetch(investmentEndpoint, {
             method: 'POST',
             headers: {
@@ -38,7 +39,7 @@ class Investments extends Component {
 
     sellHandler = async (id, purchasePrice, symbol,  event) => {
         event.preventDefault();
-        const saleEndpoint = 'https://pure-ridge-03326.herokuapp.com/myinvestments/sell';
+        const saleEndpoint = 'http://localhost:8000/myinvestments/sell';
         let response = await fetch(saleEndpoint, {
             method: 'DELETE',
             headers: {
@@ -51,8 +52,8 @@ class Investments extends Component {
         data ? console.log("Data") : console.log("no Data");
         //Phase 2: display sell data and send to state
        
-        // get latest price
-        const latestPriceEndpoint = 'https://pure-ridge-03326.herokuapp.com/myinvestments/saleprice'
+        // get latest price https://pure-ridge-03326.herokuapp.com/
+        const latestPriceEndpoint = 'http://localhost:8000/myinvestments/saleprice'
         let latestPrice = await fetch(latestPriceEndpoint, {
             method: 'POST',
             headers: {
@@ -62,7 +63,7 @@ class Investments extends Component {
         })
 
         let lastClose = await latestPrice.json();
-        // console.log('Last Close Data: ', lastClose);  Data looks like: {date: '2021-10-05', price: 141.11}
+        console.log('Last Close Data: ', lastClose);  // Data looks like: {date: '2021-10-05', price: 141.11}
         // save to state
         this.setState({
             saleData: lastClose
@@ -70,18 +71,55 @@ class Investments extends Component {
 
         // INSTRUCTIONS FOR NEXT STEPS - 10/26
         // 1) POST to sale to log sale and update the DOM
-        // 2) Set state and do math
-        // 3) Modal to display confirmations and maybe sell data from recent sale only
+        const allSaleEndpoint = 'http://localhost:8000/sale/'
+
+        let postSale = await fetch(allSaleEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                symbol: symbol,
+                lots: 100,
+                date: this.state.saleData['date'],
+                buyPrice: purchasePrice,
+                sellPrice: this.state.saleData['price'],
+                userId: sessionStorage.getItem('userId')
+            })
+        })
+
+        const result = await postSale.json();
+        console.log('The result of sale POST!!!!!: ', result)
+        // 2) Set state and do math - CALEB2
+
         // do math
         let {roi, profit} = investmentMath(purchasePrice, this.state.saleData['price'])
         this.setState({
             roi: roi,
             profit: profit
         })
-        
+        // 3) Modal to display confirmations and maybe sell data from recent sale only - CALEB3
+
         // call getInvestmenthander again to remove investment
-        this.getInvestmentsHandler();
-        // call getSaleHandler (OR whatever i call it) to update the portfolio results
+        await this.getInvestmentsHandler();
+        // call getSalesHandler to update the portfolio results
+        await this.getSalesHandler();
+    }
+// CALEBNOW - test API call!!
+    getSalesHandler = async () => {
+        const getSaleEndpoint = 'http://localhost:8000/sale/all';
+        let response = await fetch(getSaleEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({userId: sessionStorage.getItem('userId')})
+        })
+
+        const data = await response.json();
+        // update state and/or format data and do math - CALEB0
+        console.log("sales Handler post-response.json: ", data)
+        
     }
 
     handleLogout = () => {
