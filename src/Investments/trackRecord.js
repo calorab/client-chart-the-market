1fd0ac84d2fbb2dbc4f4b80d7be402a90282326e
Caleb Gammon <calebwgammon@gmail.com>
2021-11-02 12:43:18 -0400

    fixed floating decimal point issue

--- src/Investments/trackRecord.js
+++ src/Investments/trackRecord.js
@@ -1,13 +1,13 @@
 import { produceWithPatches } from 'immer';
-import react from 'react';
+import React from 'react';
 import Wrapper from '../utility/Wrapper/wrapper'
 
 const trackRecord = props => {
     return (
         <Wrapper>
             <h3>Track Record</h3>
-            <p>To-date return (%): ${props.percent}</p>
-            <p>To-Date profit ($): ${props.dollars}</p>
+            <p>To-date return (%): {props.percent.toFixed(2)}%</p>
+            <p>To-Date profit ($): ${props.dollars.toFixed(2)}</p>
         </Wrapper>
     )
 };
--- src/utility/investmentMath.js
+++ src/utility/investmentMath.js
@@ -1,7 +1,6 @@
 
 const investmentMath = (buyPrice, sellPrice) => {
-    let percentage = ((sellPrice - buyPrice) / buyPrice) * 100;
-    let roi = Math.round(percentage * 100)/100;
+    let roi = ((sellPrice - buyPrice) / buyPrice) * 100;
     let profit = sellPrice - buyPrice;
 
     return {roi, profit}
