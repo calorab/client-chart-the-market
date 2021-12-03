5ff32f61f7b1181460635858482d52c42197f5fc
Caleb Gammon <calebwgammon@gmail.com>
2021-12-03 15:59:53 -0500

    switched investments to hooks and fixed state updating issues along the way

--- src/Investments/investments.js
+++ src/Investments/investments.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import InvestmentCard from './investmentCard'
 import investmentMath from '../utility/investmentMath';
 import Button from '../UI/button';
@@ -16,28 +16,27 @@ const investmentMainStyle = {
     justifyContent: 'space-between',
 };
 
-class Investments extends Component {
-    state = {
-        investmentsArray: [],
-        soldSymbol: '',
-        latestPrice: 9,
-        saleData: {},
-        roi: 0,
-        profit: 0,
-        modal: false,
-        toDateReturn: 0,
-        toDateProfit: 0
-    }
+const Investments = (props) => {
 
-    componentDidMount() {
-        if (!sessionStorage.getItem('userId') && this.props.history) {
-            this.props.history.push('/auth');
+    const [investmentsArray,setInvestmentsArray] = useState([]);
+    const [soldSymbol,setSoldSymbol] = useState('');
+    const [saleData,setSaleData] = useState({});
+    const [roi,setRoi] = useState(0);
+    const [profit,setProfit] = useState(0);
+    const [modal,setModal] = useState(false)
+    const [toDateReturn,setToDateReturn] = useState(0);
+    const [toDateProfit,setToDateProfit] = useState(0);
+
+// use effect
+    useEffect(() => {
+        if (!sessionStorage.getItem('userId') && props.history) {
+            props.history.push('/auth');
         }
-        this.getInvestmentsHandler();
-        this.getSalesHandler();
-    }
+        getInvestmentsHandler();
+        getSalesHandler();
+    },[saleData])
 
-    getInvestmentsHandler = async () => {
+    const getInvestmentsHandler = async () => {
         const investmentEndpoint = 'https://pure-ridge-03326.herokuapp.com/myinvestments';
         let response = await fetch(investmentEndpoint, {
             method: 'POST',
@@ -48,10 +47,11 @@ class Investments extends Component {
         }).catch(err => console.log(err))
 
         const data = await response.json();
-        this.setState({investmentsArray: data})
+        setInvestmentsArray(data);
+        console.log("DATA: ",data)
     }
 
-    sellHandler = async (id, purchasePrice, symbol,  event) => {
+    const sellHandler = async (id, purchasePrice, symbol,  event) => {
         event.preventDefault();
         const saleEndpoint = 'https://pure-ridge-03326.herokuapp.com/myinvestments/sell';
         let response = await fetch(saleEndpoint, {
@@ -63,7 +63,6 @@ class Investments extends Component {
         }).catch(err => console.log(err))
 
         const data = await response.json();
-        // data ? console.log("Data") : console.log("no Data");
        
         const latestPriceEndpoint = 'https://pure-ridge-03326.herokuapp.com/myinvestments/saleprice'
         let latestPrice = await fetch(latestPriceEndpoint, {
@@ -75,13 +74,10 @@ class Investments extends Component {
         }).catch(err => console.log(err))
 
         let lastClose = await latestPrice.json();
+        // CALEB - handle 'non-errors' here!!!!!!!!!!!!
+        // 
+        console.log("lastClose at sale: ",lastClose)
         
-        this.setState({
-            saleData: lastClose
-        })
-
-        // INSTRUCTIONS FOR NEXT STEPS - 10/26
-        // 1) POST to sale to log sale and update the DOM
         const allSaleEndpoint = 'https://pure-ridge-03326.herokuapp.com/sale/'
 
         let postSale = await fetch(allSaleEndpoint, {
@@ -92,31 +88,26 @@ class Investments extends Component {
             body: JSON.stringify({
                 symbol: symbol,
                 lots: 100,
-                date: this.state.saleData['date'],
+                date: lastClose['date'],
                 buyPrice: purchasePrice,
-                sellPrice: this.state.saleData['price'],
+                sellPrice: lastClose['price'],
                 userId: sessionStorage.getItem('userId')
             })
         }).catch(err => console.log(err))
 
         const result = await postSale.json();
-        // console.log('The result of sale POST!!!!!: ', result)
+        console.log('The result of sale POST: ', result)
 
         
-        let {roi, profit} = investmentMath(purchasePrice, this.state.saleData['price'])
-        this.setState({
-            roi: roi,
-            profit: profit,
-            modal: true,
-            soldSymbol: symbol
-        })
-        
-        await this.getInvestmentsHandler();
-        
-        await this.getSalesHandler();
+        let {roi, profit} = investmentMath(purchasePrice, lastClose['price'])
+        setSoldSymbol(symbol);
+        setRoi(roi);
+        setProfit(profit);
+        setModal(true);
+        setSaleData(lastClose);
     }
 
-    getSalesHandler = async () => {
+    const getSalesHandler = async () => {
         const getSaleEndpoint = 'https://pure-ridge-03326.herokuapp.com/sale/all';
         
         let response = await fetch(getSaleEndpoint, {
@@ -128,85 +119,83 @@ class Investments extends Component {
         }).catch(err => console.log('The salesHandler error: ', err))
 
         const data = await response.json();
-       
-        // console.log("sales Handler post-response.json: ", data) ;
 
-        this.statsHandler(data);
-        // console.log('statsHandler state values (return/profit): ', this.state.toDateReturn, this.state.toDateProfit);
+        statsHandler(data);
     }
 
-    statsHandler = (arr) => {
+    const statsHandler = (arr) => {
+        console.log("the array: ", arr)
         let dollar = 0;
         let percentage = 0;
-        
+        // CALEB - check math on adding percentages - is this the right output??
         for (let i=0; i<arr.length; i++) {
             let {roi, profit} = investmentMath(arr[i]['buyPrice'], arr[i]['sellPrice']);
             dollar += profit;
             percentage += roi;
         }
-        this.setState({toDateReturn: percentage, toDateProfit: dollar})
+        
+        console.log('$$ && %%', dollar, percentage);
+        setToDateReturn(percentage);
+        setToDateProfit(dollar);
     };
 
-    handleLogout = () => {
-        this.props.history.push('/logout');
+    const handleLogout = () => {
+        props.history.push('/logout');
     };
 
-    handleModal = () => {
-        this.setState({modal: false})
+    const handleModal = () => {
+        setModal(false);
     }
 
-    render() {
+    let myInvestmentsArray = [];
+    for (const element of investmentsArray) {
+        myInvestmentsArray.push({
+            id: element._id,
+            symbol: element.equity,
+            purchaseDate: element.date,
+            purchasePrice: element.buyPrice,
+        })
+    }
+// CALEB - is this displaying correctly when no investments???
+    let emptyPortfolio = 
+    <div>
+        <h5>You have no Investments</h5>
+        <p>Return to the main page to buy stocks and see them here!</p>
+    </div>;
+    let myInvestmentResults = [];
+    if (myInvestmentsArray) {
+        myInvestmentResults = myInvestmentsArray.map(element => {
+            return <InvestmentCard 
+                key={element.id}
+                purchaseDate={element.purchaseDate}
+                purchasePrice={element.purchasePrice}
+                clicked={event => sellHandler(element.id, element.purchasePrice, element.symbol, event)}> 
+                {element.symbol}
+            </InvestmentCard>
+        });
+    }
 
-        let myInvestmentsArray = [];
-        for (const element of this.state.investmentsArray) {
-            myInvestmentsArray.push({
-                id: element._id,
-                symbol: element.equity,
-                purchaseDate: element.date,
-                purchasePrice: element.buyPrice,
-            })
-        }
+    const sellMessage = `You have sold ${soldSymbol} stock for $${saleData.price}, netting $${profit.toFixed()} per share or ${roi.toFixed()}%`;
 
-        let myInvestmentResults = 
-        <div>
-            <h5>You have no Investments</h5>
-            <p>Return to the main page to buy stocks and see them here!</p>
-        </div>;
-
-        if (this.state.investmentsArray) {
-            myInvestmentResults = myInvestmentsArray.map(element => {
-                return <InvestmentCard 
-                    key={element.id}
-                    purchaseDate={element.purchaseDate}
-                    purchasePrice={element.purchasePrice}
-                    clicked={event => this.sellHandler(element.id, element.purchasePrice, element.symbol, event)}> 
-                    {element.symbol}
-                </InvestmentCard>
-            });
-        }
-
-        const sellMessage = `You have sold ${this.state.soldSymbol} stock for $${this.state.saleData.price}, netting $${this.state.profit.toFixed(2)} per share or ${this.state.roi.toFixed(2)}%`;
-
-        return (
-            <Wrapper>
-                {this.state.modal ? <Modal title={`${this.state.soldSymbol} Stock Sold!`} message={sellMessage} onConfirm={this.handleModal}></Modal> : null}
-                <div style={investmentMainStyle}>
-                    <div className={styles.investResults}>
-                        <div className={styles.title}>
-                            <h5>Your Portfolio</h5> 
-                        </div>
-                        {myInvestmentResults}
+    return (
+        <Wrapper>
+            {modal ? <Modal title={`${soldSymbol} Stock Sold!`} message={sellMessage} onConfirm={handleModal}></Modal> : null}
+            <div style={investmentMainStyle}>
+                <div className={styles.investResults}>
+                    <div className={styles.title}>
+                        <h5>Your Portfolio</h5> 
                     </div>
-                    <div className={styles.line}></div>
-                    <div className={styles.investNav}>  
-                        <TrackRecord percent={this.state.toDateReturn} dollars={this.state.toDateProfit}/>
-                        <Button clicked={this.handleLogout}>Logout</Button>
-                        <Button type='submit' clicked={event => this.props.history.push('/')}>Return to chart</Button>
-                    </div>
+                    {myInvestmentResults}
                 </div>
-            </Wrapper>
-        )
-    }
+                <div className={styles.line}></div>
+                <div className={styles.investNav}>  
+                    <TrackRecord percent={toDateReturn} dollars={toDateProfit}/>
+                    <Button clicked={handleLogout}>Logout</Button>
+                    <Button type='submit' clicked={event => props.history.push('/')}>Return to chart</Button>
+                </div>
+            </div>
+        </Wrapper>
+    )
 }
 
 export default Investments;
